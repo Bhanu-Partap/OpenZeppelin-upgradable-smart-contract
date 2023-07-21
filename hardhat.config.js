0befe52ffa4a833b2dc5dea0c548c7b091ba084c
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-21 10:23:11 +0530

    openzepplin upgradable contract

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require("@nomicfoundation/hardhat-toolbox");
+// hardhat.config.js
+require('@openzeppelin/hardhat-upgrades');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+};
