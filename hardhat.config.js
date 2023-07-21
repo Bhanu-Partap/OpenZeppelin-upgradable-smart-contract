ae587b874e23740604dca60436151704b42b3caa
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-21 14:53:36 +0530

    upgradable smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,7 @@
-// require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox");
 // hardhat.config.js
 require("@openzeppelin/hardhat-upgrades");
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,10 +9,13 @@ module.exports = {
   networks: {
     sepolia: {
       url: "https://sepolia.infura.io/v3/ace13403824d4f809e011d9d547a4aa1",
-      accounts: [process.env.PRI_KEY],
+      accounts: [`56fe1fd10f0203c190640fb0bad78e9bf29d10fc2fbc97ee1ff49ee4acd2d08a`],
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      sepolia : "ace13403824d4f809e011d9d547a4aa1"
+    }
+    
   },
 };
--- scripts/deploy_v1.js
+++ scripts/deploy_v1.js
@@ -3,10 +3,17 @@ const {ethers, upgrades} = require("hardhat")
 async function main(){
     const Box = await ethers.getContractFactory("Box")
 
-    const box = await upgrades.deployProxy(Box, [10], {
-        initializer : "initialize"
+    const box = await upgrades.deployProxy(Box,[10],{
+        initializer : "intialize"
     })
     await box.waitForDeployment();
+
+    const address = await ("Box contract deployed at ", box.getAddress());
+    console.log(address);
 }  
 
-main()
+main();
+
+
+// contract  1address
+//   x89d5175802A0aCEC987a078b077460CB09AfEa9f
