2934698fbac25fb9bd86c0da4b4afe425b9a0320
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-21 11:08:08 +0530

    upgradable contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 // hardhat.config.js
-require('@openzeppelin/hardhat-upgrades');
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.10",
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/ace13403824d4f809e011d9d547a4aa1",
+      accounts: [process.env.PRI_KEY],
+    },
+  },
 };
