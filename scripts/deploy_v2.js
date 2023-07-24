ba4f4be5ca42535c329674c2fe11d90e81b17d7d
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-24 12:25:36 +0530

    v2

--- scripts/deploy_v2.js
+++ scripts/deploy_v2.js
@@ -2,7 +2,10 @@ const {ethers, upgrades} = require("hardhat")
 
 async function main(){
     const BoxV2 = await ethers.getContractFactory("BoxV2")
-
+    const boxv2 = await upgrades.deployProxy(BoxV2, {
+        initializer : "inc"
+    })
+    
    
 }  
 
