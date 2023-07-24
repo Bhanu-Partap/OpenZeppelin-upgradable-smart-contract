408f11b79a17dfe48f1b605e4889c8abebc3a1c3
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-24 12:25:16 +0530

    v2

---
+++ scripts/deploy_v2.js
@@ -0,0 +1,13 @@
+const {ethers, upgrades} = require("hardhat")
+
+async function main(){
+    const BoxV2 = await ethers.getContractFactory("BoxV2")
+
+   
+}  
+
+main();
+
+
+// contract  1address
+//   x89d5175802A0aCEC987a078b077460CB09AfEa9f
