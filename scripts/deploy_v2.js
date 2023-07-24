aebdd9083fd7c0e12bbd114395a686183bc7f75d
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-24 17:00:44 +0530

    upgradable smart contract

--- scripts/deploy_v2.js
+++ scripts/deploy_v2.js
@@ -2,9 +2,7 @@ const {ethers, upgrades} = require("hardhat")
 
 async function main(){
     const BoxV2 = await ethers.getContractFactory("BoxV2")
-    const boxv2 = await upgrades.upgradeProxy("0x69D020BD579fdFE174aF46009F0503feE6ece4Eb", {
-        initializer : "inc"
-    })
+    const boxv2 = await upgrades.upgradeProxy("0x69D020BD579fdFE174aF46009F0503feE6ece4Eb",BoxV2)
     await boxv2.waitForDeployment();
 
     const address = await ("Box contract deployed at ", boxv2.getAddress());
